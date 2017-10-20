bc5f55e0428ba80ced43af8e76f83e428fa12796
Dalia Galván <idalikg@gmail.com>
2017-10-19 22:10:02 -0500

    Primer servidor Se creó la carpeta '01_primer_servidor' en el cual se incluyen ejemplos sobre los primeros pasos para la creación de un servidor en NodeJS utilizando Express.js.

---
+++ 01_primer_servidor/app_1.js
@@ -0,0 +1,10 @@
+// Imporción del módulo express
+const express = require('express');
+
+// Inicializando servidor
+const app = express();
+
+// Corriendo servidor en el puerto 8000
+app.listen(8000);
+
+console.log('Primer ejemplo de servidor en Express');
